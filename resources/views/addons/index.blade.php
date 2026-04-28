558acc056f59481e07f842edd384f8434ce143bc
MAC <mac@MACs-MacBook-Pro.local>
2026-04-28 03:08:39 +0100

    push

--- resources/views/addons/index.blade.php
+++ resources/views/addons/index.blade.php
@@ -49,7 +49,7 @@
                             @if($addon->price <= 0)
                                 <span class="bg-green-600 text-white text-xs font-bold px-3 py-1 rounded-full">Free</span>
                             @else
-                                <span class="bg-purple-600 text-white text-xs font-bold px-3 py-1 rounded-full">
+                                <span class="bg-purple-600 text-white text-xs font-bold px-3 py-1 rounded-full" style="text-decoration: line-through">
                                     ${{ number_format($addon->price, 2) }}
                                 </span>
                                 @if($addon->original_price && $addon->original_price > $addon->price)
